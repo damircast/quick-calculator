3578dbca19eefa5abcbf3d7d0c3b359bb94f5369
dmrlcc <d.lucic@alsterspree.de>
2023-03-29 21:15:22 +0200

    Added calculations for summarize and substract

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
     const divParent = document.createElement("div");
     const divHistory = document.createElement("div");
     const div1 = document.createElement("div");
-    const div2 = document.createElement("div");
+    const divResult = document.createElement("div");
     
     const button1 = document.createElement("button");
     const button2 = document.createElement("button");
@@ -13,7 +13,7 @@
     divParent.setAttribute("id","calc-parent");
     divHistory.setAttribute("id","calc-history");
     div1.setAttribute("id","calc-button");
-    div2.setAttribute("id","finalResult");
+    divResult.setAttribute("id","finalResult");
     
     button1.setAttribute("id","add");
     button2.setAttribute("id","subtract");
@@ -28,14 +28,20 @@
     document.body.appendChild(divParent)
     divParent.appendChild(divHistory)
     divParent.appendChild(div1)
-    divParent.appendChild(div2)
+    divParent.appendChild(divResult)
     div1.appendChild(button1)
     div1.appendChild(button2)
     div1.appendChild(button3)
     div1.appendChild(button4)
 
+// Buttons
+const add = document.getElementById("add");
+const subtract = document.getElementById("subtract");
+const divide = document.getElementById("divide");
+const multiply = document.getElementById("multiply");
+
 // States
-let selection, finalResult
+let selection, finalResult, calculation, sum
 let allSelections = []
 
 document.addEventListener('mouseup', function() {
@@ -44,16 +50,28 @@ document.addEventListener('mouseup', function() {
         let selectionToNumber = Math.floor(selection) // Convert string to number
         allSelections.push(selectionToNumber)
         console.log(allSelections)
-        window.getSelection().empty();
+        window.getSelection().empty(); // Remove selection (in Chrome only?)
     }
 });
 
+// Addition
 add.addEventListener('click', () => {
-    // if (window.getSelection().empty) {  // Chrome
-        window.getSelection().empty();
-    // }
+    window.getSelection().empty(); // Remove selection (in Chrome only?)
     sum = allSelections.reduce((a, b) => a + b, 0) // Summarize all numbers in the array
-    div2.innerText = sum
+    divResult.innerText = sum
+    allSelections.length = 0 // Clear array before summarizing previous to avoid double value
+    allSelections.push(sum)
+})
+
+// Substraction
+subtract.addEventListener('click', () => {
+    window.getSelection().empty(); // Remove selection (in Chrome only?)
+    let firstNumber = allSelections.shift()
+    sum = allSelections.reduce((a, b) => a + b, 0) // Summarize all numbers in the array without the first
+    calculation = firstNumber - sum
+    divResult.innerText = calculation
+    allSelections.length = 0
+    allSelections.push(calculation)
 })
 
 // selectionOne has to be an array with ALL numbers multiplied together. If one number is only in array, use just one. 
